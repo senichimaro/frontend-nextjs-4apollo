c897a37612f123367c7f6053512a2774229e97ee
senichi <senichimaro@gmail.com>
2023-11-23 12:14:50 +0100

    init repo

---
+++ src/pages/_app.js
@@ -0,0 +1,16 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+import '../styles.css'
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache()
+})
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
